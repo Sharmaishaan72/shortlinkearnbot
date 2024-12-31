7f55e4c221c8744def71bc1deea03fe9d1e13790
Ishaan Sharma <161037269+Sharmaishaan72@users.noreply.github.com>
2024-12-31 13:49:44 +0530

    Update balanceadder.py

--- handlers/balanceadder.py
+++ handlers/balanceadder.py
@@ -1,5 +1,4 @@
 import quart
-import ngrok
 import os
 import requests
 
@@ -17,8 +16,5 @@ def checkshortlink(random_code):
     return reward
 
 async def runapp():
-    ngrok.set_auth_token(os.getenv("ngrok_token"))
-    listener = await ngrok.forward(5000,hostname=os.getenv("ngrok_url"))
-    print(listener.url())
     await app.run_task(host='0.0.0.0',port=5000)
 
